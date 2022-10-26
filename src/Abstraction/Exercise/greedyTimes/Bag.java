701cc67fd9bd0f54978cbd44d42fad5876ba9118
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-10-26 16:46:59 +0300

    OOP

---
+++ src/Abstraction/Exercise/greedyTimes/Bag.java
@@ -0,0 +1,93 @@
+package Abstraction.Exercise.greedyTimes;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Bag {
+    private long capacity;
+
+
+    private Map<String, Map<String, Long>> item;
+    private long currentCapacity;
+
+    public Bag(long capacity) {
+        this.capacity = capacity;
+        this.item = new HashMap<>();
+
+    }
+
+    public void addCash(String name, long countOfItem) {
+        if (countOfItem + currentAmountOfItem(this.item, "Cash") <= capacity && countOfItem + currentAmountOfItem(this.item, "Cash") <= currentAmountOfItem(this.item, "Gem")) {
+            if (!this.item.containsKey("Cash")) {
+                this.item.put("Cash", new HashMap<>());
+                this.item.get("Cash").put(name, countOfItem);
+            } else {
+                this.item.get("Cash").put(name, countOfItem + currentAmountOfItem(this.item, name));
+            }
+            currentCapacity += countOfItem;
+        }
+    }
+
+    public void addGold(String name, long countOfItem) {
+        if (countOfItem + currentAmountOfItem(this.item, name) <= capacity) {
+            if (!this.item.containsKey("Gold")) {
+                this.item.put(name, new HashMap<>());
+                this.item.get(name).put(name, countOfItem);
+            } else {
+                this.item.get(name).put("Gold", countOfItem + currentAmountOfItem(this.item, name));
+            }
+            currentCapacity += countOfItem;
+        }
+    }
+
+    public void addGem(String name, long countOfItem) {
+        if (countOfItem + currentAmountOfItem(this.item, "Gem") <= capacity && countOfItem + currentAmountOfItem(this.item, "Gem") <= currentAmountOfItem(this.item, "Gold")) {
+            if (!this.item.containsKey("Gem")) {
+                this.item.put("Gem", new HashMap<>());
+                this.item.get("Gem").put(name, countOfItem);
+            } else {
+                this.item.get("Gem").put(name, countOfItem + currentAmountOfItem(this.item, name));
+            }
+            currentCapacity += countOfItem;
+        }
+    }
+
+    private long currentAmountOfItem(Map<String, Map<String, Long>> item, String name) {
+        return item.entrySet().stream().filter(e -> e.getKey().equals(name)).mapToLong(p -> p.getValue().values().stream().mapToLong(x -> x).sum()).sum();
+    }
+
+    public Map<String, Map<String, Long>> getItem() {
+        return item;
+    }
+
+    public void printContent() {
+        Map<String, Map<String, Long>> newMap = new LinkedHashMap<>();
+        this.item.entrySet()
+                .stream()
+                .sorted((left, right) -> {
+                    long leftSum = left.getValue().values().stream().mapToLong(l -> l).sum();
+                    long rightSum = right.getValue().values().stream().mapToLong(l -> l).sum();
+                    if (rightSum > leftSum){
+                        return 1;
+                    }else if (rightSum< leftSum){
+                        return -1;
+                    }else {
+                        return 0;
+                    }
+                }).forEach(elements -> newMap.put(elements.getKey(), elements.getValue()));
+        for (Map.Entry<String, Map<String, Long>> entry : newMap.entrySet()) {
+            Long sumValues = entry.getValue().values().stream().mapToLong(l -> l).sum();
+
+            System.out.printf("<%s> $%s%n", entry.getKey(), sumValues);
+
+            entry.getValue().entrySet().stream().sorted((e1, e2) -> e2.getKey().compareTo(e1.getKey())).forEach(i -> System.out.println("##" + i.getKey() + " - " + i.getValue()));
+        }
+
+
+    }
+}
+
+
+
--- src/Abstraction/Exercise/greedyTimes/Main.java
+++ src/Abstraction/Exercise/greedyTimes/Main.java
@@ -13,84 +13,22 @@ public class Main {
         long capacityOfBag = Long.parseLong(scanner.nextLine());
         String[] items = scanner.nextLine().split("\\s+");
 
-
-        Map<String, Map<String, Long>> bagOfItems = new LinkedHashMap<>();
-        long gold = 0;
-        long gem = 0;
-        long money = 0;
+        Bag bag = new Bag(capacityOfBag);
 
         for (int i = 0; i < items.length; i += 2) {
             String name = items[i];
             long countOfItem = Long.parseLong(items[i + 1]);
 
-            String currentItem = "";
 
             if (name.length() == 3) {
-                currentItem = "Cash";
+                bag.addCash(name, countOfItem);
             } else if (name.toLowerCase().endsWith("gem")) {
-                currentItem = "Gem";
+                bag.addGem(name, countOfItem);
             } else if (name.toLowerCase().equals("gold")) {
-                currentItem = "Gold";
-            }
-
-            if (currentItem.equals("")) {
-                continue;
-            } else if (capacityOfBag < bagOfItems.values().stream().map(Map::values).flatMap(Collection::stream).mapToLong(e -> e).sum() + countOfItem) {
-                continue;
-            }
-
-            switch (currentItem) {
-                case "Gem":
-                    if (checkItem(bagOfItems, currentItem, "Gold", countOfItem)) continue;
-                    break;
-                case "Cash":
-                    if (checkItem(bagOfItems, currentItem, "Gem", countOfItem)) continue;
-                    break;
-            }
-
-            if (!bagOfItems.containsKey(currentItem)) {
-                bagOfItems.put((currentItem), new LinkedHashMap<String, Long>());
-            }
-
-            if (!bagOfItems.get(currentItem).containsKey(name)) {
-                bagOfItems.get(currentItem).put(name, 0L);
-            }
-            bagOfItems.get(currentItem).put(name, bagOfItems.get(currentItem).get(name) + countOfItem);
-            switch (currentItem) {
-                case "Gold":
-                     gold += countOfItem;
-                     break;
-                case "Gem":
-                     gem += countOfItem;
-                     break;
-                case "Cash":
-                    money += countOfItem;
-                    break;
+                bag.addGold(name, countOfItem);
             }
         }
+        bag.printContent();
 
-        for (Map.Entry<String, Map<String, Long>> entry : bagOfItems.entrySet()) {
-            Long sumValues = entry.getValue().values().stream().mapToLong(l -> l).sum();
-
-            System.out.printf("<%s> $%s%n", entry.getKey(), sumValues);
-
-            entry.getValue().entrySet().stream().sorted((e1, e2) -> e2.getKey().compareTo(e1.getKey())).forEach(i -> System.out.println("##" + i.getKey() + " - " + i.getValue()));
-
-        }
-    }
-
-    private static boolean checkItem(Map<String, Map<String, Long>> bagOfItems, String currentItem, String item, long countOfItem) {
-        if (!bagOfItems.containsKey(currentItem)) {
-            if (bagOfItems.containsKey(item)) {
-                if (countOfItem > bagOfItems.get("Gold").values().stream().mapToLong(e -> e).sum()) {
-                    return true;
-                }
-            } else {
-                return true;
-            }
-        } else if (bagOfItems.get(currentItem).values().stream().mapToLong(e -> e).sum() + countOfItem > bagOfItems.get(item).values().stream().mapToLong(e -> e).sum()) {
-            return true;
-        }
-        return false;
     }
 }
