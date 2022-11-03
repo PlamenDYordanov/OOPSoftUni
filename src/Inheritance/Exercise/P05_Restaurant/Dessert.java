23c1a64598436ee809adeb0745dc7b60d0e15910
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-11-03 21:25:15 +0200

    OOP

---
+++ src/Inheritance/Exercise/P05_Restaurant/Dessert.java
@@ -0,0 +1,15 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class Dessert extends Food{
+    private double calories;
+    public Dessert(String name, BigDecimal price, double gram, double calories) {
+        super(name, price, gram);
+        this.calories = calories;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+}
