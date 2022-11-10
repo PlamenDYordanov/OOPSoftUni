2b1f1ff8f4d38b94822641ca536342e3de8471de
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-11-10 19:32:11 +0200

    OOP

---
+++ src/Polymorphism/Lab/P01_MathOperation/MathOperation.java
@@ -0,0 +1,16 @@
+package Polymorphism.Lab.P01_MathOperation;
+
+public class MathOperation {
+
+    public int add(int i1, int i2) {
+      return   i1 + i2;
+    }
+
+    public int add(int i1, int i2, int i3) {
+        return i1 + i2 + i3;
+    }
+
+    public int add(int i1, int i2, int i3, int i4) {
+        return i1+ i2 + i3 + i4;
+    }
+}
