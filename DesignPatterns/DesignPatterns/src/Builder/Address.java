741540c793c4f584dfcb69c38d46df565fe7e504
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-11-29 23:26:16 +0200

    OOP

---
+++ DesignPatterns/DesignPatterns/src/Builder/Address.java
@@ -0,0 +1,35 @@
+package Builder;
+
+public class Address {
+    private String country;
+    private String city;
+
+    public Address(String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+
+    public static Address.Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String country;
+        private String city;
+
+        public Builder withCountry(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public Builder withCity(String city) {
+            this.city = city;
+            return this;
+        }
+        public Address build() {
+            return new Address(this.country, this.city);
+        }
+    }
+
+
+}
---
+++ DesignPatterns/DesignPatterns/src/Builder/Main.java
@@ -0,0 +1,11 @@
+package Builder;
+
+public class Main {
+    public static void main(String[] args) {
+        Address address = new Address("Bulgaria", "Varna");
+        Address address1 = Address.builder()
+                .withCity("Sofia")
+                .withCountry("Bulgaria")
+                .build();
+    }
+}
