074ee7c82fb8b5da609ad8550a03ed10247bb41f
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-12-02 13:37:32 +0200

    OOP

--- ExamPrerp/P02_August2022/FootballTeam/src/test/java/football/FootballTeamTests.java
+++ ExamPrerp/P02_August2022/FootballTeam/src/test/java/football/FootballTeamTests.java
@@ -1,5 +1,74 @@
 package football;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
 public class FootballTeamTests {
+
+   private static final Footballer footballer1 = new Footballer("Plamen");
+   private static final Footballer footballer2 = new Footballer("Ivan");
+   private static final Footballer footballer3 = new Footballer("Dragan");
+   FootballTeam footballTeam;
+
+   @Before
+    public void prepareTest() {
+       footballTeam = new FootballTeam("Arsenal", 2);
+       footballTeam.addFootballer(footballer1);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+    public void testAddFootballerOutOfCapacityThrowException() {
+       footballTeam.addFootballer(footballer2);
+       footballTeam.addFootballer(footballer3);
+   }
+   @Test (expected =  IllegalArgumentException.class)
+   public void testSetVacantPointsNegativeNumberThrowException(){
+      footballTeam = new FootballTeam("Arsenal", -1);
+   }
+   @Test
+   public void testGetName() {
+      Assert.assertEquals("Arsenal", footballTeam.getName());
+   }
+   @Test(expected = NullPointerException.class)
+   public void testSetNameThrowExceptionNullOrEmpty() {
+      footballTeam = new FootballTeam(null, 2);
+   }
+   @Test
+   public void testGetCount() {
+      Assert.assertEquals(1, footballTeam.getCount());
+   }
+   @Test
+   public void testVacantPositions (){
+      Assert.assertEquals(2, footballTeam.getVacantPositions());
+   }
+   @Test(expected = IllegalArgumentException.class)
+   public void testRemoveFootballerThrowException(){
+      footballTeam.removeFootballer("Kiro");
+   }
+   @Test
+   public void testRemoveFootballer() {
+      footballTeam.removeFootballer("Plamen");
+      Assert.assertEquals(0,footballTeam.getCount());
+   }
+   @Test(expected = IllegalArgumentException.class)
+   public void testFootballerForSaleThrowException() {
+      footballTeam.footballerForSale("Kiro");
+   }
+   @Test
+   public void testFootballerForSale() {
+      footballTeam.footballerForSale("Plamen");
+      Assert.assertFalse(footballer1.isActive());
+   }
+   @Test
+   public void testGetStatistic() {
+      Assert.assertEquals(String.format("The footballer %s is in the team %s.", "Plamen", footballTeam.getName()),
+              footballTeam.getStatistics());
+   }
+
+
+
 
 }
