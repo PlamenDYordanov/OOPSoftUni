0206b80e68b960e001820c7f47aa62bd18714a87
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-12-09 10:28:45 +0200

    OOP

--- ExamPrerp/P05_DecemberRetake2021/Garage/src/test/java/garage/GarageTests.java
+++ ExamPrerp/P05_DecemberRetake2021/Garage/src/test/java/garage/GarageTests.java
@@ -1,8 +1,40 @@
 package garage;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 public class GarageTests {
-    //TODO: Test Garage class
+    private static final Car commonCar = new Car("MAZDA", 200, 5000);
+    private static final Car sportCar = new Car("BMW", 300, 10000);
+    private static final Car muscleCar = new Car("FORD", 320, 20000);
+    Garage garage;
+
+    @Before
+    public void prepareTest() {
+        garage = new Garage();
+        garage.addCar(commonCar);
+    }
+
+    @Test
+    public void testGetCars() {
+        List<Car> cars = garage.getCars();
+        Assert.assertEquals("MAZDA",cars.get(0).getBrand());
+    }
+    @Test
+    public void testGetCount() {
+        Assert.assertEquals(1, garage.getCount());
+    }
+    @Test
+    public void testFindAllCarsWithMaxSpeedAbove() {
+        garage.addCar(muscleCar);
+        List<Car> allCarsWithMaxSpeedAbove = garage.findAllCarsWithMaxSpeedAbove(300);
+        Assert.assertEquals("FORD", allCarsWithMaxSpeedAbove.get(0).getBrand());
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddCarThrowException() {
+        garage.addCar(null);
+    }
 }
