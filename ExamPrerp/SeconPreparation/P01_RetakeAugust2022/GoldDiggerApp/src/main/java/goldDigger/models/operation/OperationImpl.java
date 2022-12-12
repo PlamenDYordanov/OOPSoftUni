658f8cf8c874ec80bebe6cd60f0d6c57076fdb13
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-12-12 13:08:45 +0200

    OOP

--- ExamPrerp/SeconPreparation/P01_RetakeAugust2022/GoldDiggerApp/src/main/java/goldDigger/models/operation/OperationImpl.java
+++ ExamPrerp/SeconPreparation/P01_RetakeAugust2022/GoldDiggerApp/src/main/java/goldDigger/models/operation/OperationImpl.java
@@ -2,20 +2,22 @@ package goldDigger.models.operation;
 
 import goldDigger.models.discoverer.Discoverer;
 import goldDigger.models.spot.Spot;
-import goldDigger.models.spot.SpotImpl;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 public class OperationImpl implements Operation{
     @Override
     public void startOperation(Spot spot, Collection<Discoverer> discoverers) {
+        Iterator<String> iterator = spot.getExhibits().iterator();
+
+
         for (Discoverer discoverer : discoverers) {
-                for (String exhibit : spot.getExhibits()) {
-                    while (discoverer.canDig()){
-                        discoverer.dig();
-                        discoverer.getMuseum().getExhibits().add(exhibit);
-                        spot.getExhibits().remove(exhibit);
-                }
+            while (discoverer.canDig() && iterator.hasNext()) {
+                discoverer.dig();
+                String currentExhibit = iterator.next();
+                discoverer.getMuseum().getExhibits().add(currentExhibit);
+                iterator.remove();
             }
         }
 
